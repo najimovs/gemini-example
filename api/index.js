17db3d6f02b255df6b69d73673fcec112fd7050e
Muhammadxon Najimov <129692531+najimovs@users.noreply.github.com>
2026-01-31 18:38:36 +0500

    Setup OpenAI

--- api/index.js
+++ api/index.js
@@ -6,18 +6,30 @@ const PORT = process.env.PORT || 3_000
 
 const app = express()
 
+app.use( express.json() )
+
 const client = new OpenAI( {
 	apiKey: process.env.GROQ_API_KEY,
 	baseURL: "https://api.groq.com/openai/v1",
 } )
-
-console.log( client )
 
 app.use( cors() )
 
 app.get( "/", ( req, res ) => {
 
 	res.send( { ok: true } )
+} )
+
+app.post( "/prompt", async ( req, res ) => {
+
+	const { prompt } = req.body
+
+	const response = await client.responses.create( {
+		model: "openai/gpt-oss-20b",
+		input: prompt,
+	} )
+
+	res.send( { answer: response.output_text } )
 } )
 
 app.listen( PORT, () => console.log( `Server ready at: ${ PORT }`) )
