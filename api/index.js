d675cc08edbbf657f7ef8c9f6c96674b29f3bfab
Muhammadxon Najimov <129692531+najimovs@users.noreply.github.com>
2026-01-31 18:21:05 +0500

    Setup frontend

--- api/index.js
+++ api/index.js
@@ -1,11 +1,13 @@
 import express from "express"
 import cors from "cors"
-import * as genai from "@google/genai"
-
-console.log( genai )
+import { GoogleGenAI } from "@google/genai"
 
 const PORT = process.env.PORT || 3_000
+const GEMINI_API_KEY = process.env.GEMINI_API_KEY
+
 const app = express()
+
+const ai = new GoogleGenAI( { apiKey: GEMINI_API_KEY } )
 
 app.use( cors() )
 
---
+++ web/src/main.js
@@ -0,0 +1,4 @@
+const response = await fetch( "http://localhost:3000" )
+const json = await response.json()
+
+console.log( json )
