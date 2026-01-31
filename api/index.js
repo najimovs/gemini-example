fb7ba028ae402c93a2953d26566d6543be57def0
Muhammadxon Najimov <129692531+najimovs@users.noreply.github.com>
2026-01-31 17:53:00 +0500

    Add express & cors

--- api/index.js
+++ api/index.js
@@ -1,1 +1,14 @@
-console.log( process.env.GEMINI_API_KEY )
+import express from "express"
+import cors from "cors"
+
+const PORT = process.env.PORT || 3_000
+const app = express()
+
+app.use( cors() )
+
+app.get( "/", ( req, res ) => {
+
+	res.send( { ok: true } )
+} )
+
+app.listen( PORT, () => console.log( `Server ready at: ${ PORT }`) )
